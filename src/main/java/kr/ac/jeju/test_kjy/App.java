4b9589bfad4170ad30fc569f55a7f1caa70e3cbe
김주영 <grasia08@gmail.com>
2012-10-05 17:12:41 +0900

    to modifiy
    add name

--- src/main/java/kr/ac/jeju/test_kjy/App.java
+++ src/main/java/kr/ac/jeju/test_kjy/App.java
@@ -7,7 +7,7 @@ package kr.ac.jeju.test_kjy;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    { 
+        System.out.println( "Hello World! jy" );
     }
 }
