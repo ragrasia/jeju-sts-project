11c4faf251043fe395fe667045b58c88d4061227
김주영 <grasia08@gmail.com>
2012-10-05 17:04:09 +0900

    init project
    des first input

---
+++ src/main/java/kr/ac/jeju/test_kjy/App.java
@@ -0,0 +1,13 @@
+package kr.ac.jeju.test_kjy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
